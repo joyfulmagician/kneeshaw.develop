72ad72128fb519dd980bc1e06259f41b1b014555
truthfuldev <truthfuldev@gmail.com>
2023-11-14 06:37:29 +0100

    complete homepage and blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import { SITE_TITLE } from "@/utils/constants";
+import GameView from "@/components/homepage/GameView";
 
 export const metadata: Metadata = {
   title: `${SITE_TITLE} - Blog`,
@@ -11,9 +12,17 @@ export const metadata: Metadata = {
 
 export default function Marketplace() {
   return (
-    <main className="flex min-h-screen w-full flex-col">
+    <main className="flex min-h-screen flex-col items-center bg-[#211D19] text-[#EDF1F3]">
       <Header />
-
+      <h2 className="mt-[146px] text-[40px] font-bold">Our Blog</h2>
+      <div className="mt-[56px]">pagination1</div>
+      <div className="mt-[58px] text-[20px] font-light">
+        Insights, News, and Tips to Level Up Your Gaming Experience
+      </div>
+      <div className="mt-[120px]">
+        <GameView />
+        <div className="mt-[60px] flex justify-center">pagination2</div>
+      </div>
       <Footer />
     </main>
   );
---
+++ src/components/homepage/GameView.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import ImageCard from "./ImageCard";
+
+export default function GameView() {
+  const cards = [
+    {
+      image: "/images/card1.png",
+      title: "Content update 0.4.2",
+      subtitle: "Patch notes",
+      paragraph:
+        "Forgotten Shore and the Dauntless Few Please Wishlist Titan Saga on Steam: Titan Saga: Chains of KronosOr purchase and play on VoxPop Games: Titan Saga: Chains of Kronos (ACT 1)...",
+    },
+    {
+      image: "/images/card2.png",
+      title: "Content update 0.4.2",
+      subtitle: "Patch notes",
+      paragraph:
+        "Forgotten Shore and the Dauntless Few Please Wishlist Titan Saga on Steam: Titan Saga: Chains of KronosOr purchase and play on VoxPop Games: Titan Saga: Chains of Kronos (ACT 1)...",
+    },
+    {
+      image: "/images/card3.png",
+      title: "Content update 0.4.2",
+      subtitle: "Patch notes",
+      paragraph:
+        "Forgotten Shore and the Dauntless Few Please Wishlist Titan Saga on Steam: Titan Saga: Chains of KronosOr purchase and play on VoxPop Games: Titan Saga: Chains of Kronos (ACT 1)...",
+    },
+    {
+      image: "/images/card4.png",
+      title: "Content update 0.4.2",
+      subtitle: "Patch notes",
+      paragraph:
+        "Forgotten Shore and the Dauntless Few Please Wishlist Titan Saga on Steam: Titan Saga: Chains of KronosOr purchase and play on VoxPop Games: Titan Saga: Chains of Kronos (ACT 1)...",
+    },
+    {
+      image: "/images/card5.png",
+      title: "Content update 0.4.2",
+      subtitle: "Patch notes",
+      paragraph:
+        "Forgotten Shore and the Dauntless Few Please Wishlist Titan Saga on Steam: Titan Saga: Chains of KronosOr purchase and play on VoxPop Games: Titan Saga: Chains of Kronos (ACT 1)...",
+    },
+    {
+      image: "/images/card6.png",
+      title: "Content update 0.4.2",
+      subtitle: "Patch notes",
+      paragraph:
+        "Forgotten Shore and the Dauntless Few Please Wishlist Titan Saga on Steam: Titan Saga: Chains of KronosOr purchase and play on VoxPop Games: Titan Saga: Chains of Kronos (ACT 1)...",
+    },
+    {
+      image: "/images/card7.png",
+      title: "Content update 0.4.2",
+      subtitle: "Patch notes",
+      paragraph:
+        "Forgotten Shore and the Dauntless Few Please Wishlist Titan Saga on Steam: Titan Saga: Chains of KronosOr purchase and play on VoxPop Games: Titan Saga: Chains of Kronos (ACT 1)...",
+    },
+    {
+      image: "/images/card8.png",
+      title: "Content update 0.4.2",
+      subtitle: "Patch notes",
+      paragraph:
+        "Forgotten Shore and the Dauntless Few Please Wishlist Titan Saga on Steam: Titan Saga: Chains of KronosOr purchase and play on VoxPop Games: Titan Saga: Chains of Kronos (ACT 1)...",
+    },
+  ];
+
+  return (
+    <div className="mx-[40px] flex w-full flex-col">
+      <div className="grid grid-cols-1 gap-[40px] md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {cards.map(({ image, title, subtitle, paragraph }, index) => (
+          <ImageCard
+            key={index}
+            image={image}
+            title={title}
+            subtitle={subtitle}
+            paragraph={paragraph}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
