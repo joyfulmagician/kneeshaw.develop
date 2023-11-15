3b21b3017964a8e37496a2590f72d7e730e50c32
JoyfulMagician <joyfulmagician@gmail.com>
2023-11-15 18:48:12 +0100

    create terms, privacy, cookies

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,271 @@
+import { Metadata } from "next";
+
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+import { SITE_TITLE } from "@/utils/constants";
+
+export const metadata: Metadata = {
+  title: `${SITE_TITLE} - Privacy`,
+  description: `${SITE_TITLE} - Privacy`,
+};
+
+export default function Privacy() {
+  return (
+    <main className="flex min-h-screen w-full flex-col bg-[#211D19] text-white">
+      <Header />
+
+      <section className="mt-[146px] flex flex-col items-center">
+        <h1 className="text-[40px] font-[700] text-[#EDF1F3]">About Us</h1>
+        <img
+          src="/images/aboutus_lineimage_01.svg"
+          className="mt-[56px] w-[100px]"
+        />
+      </section>
+
+      <section className="mt-[91px] flex justify-center text-center">
+        <h3 className="w-[966px] text-[20px] font-[400] text-[#EDF1F3]">
+          Crafting unforgettable gaming experiences with innovative design and
+          cutting-edge technology
+        </h3>
+      </section>
+
+      <section className="mt-[160px] flex flex-col 2xl:flex-row">
+        <div className="w-full 2xl:w-1/2 ">
+          <img src="/images/aboutus_lineimage_02.svg" className="w-[100px]" />
+          <h1 className="mt-[19px] text-center text-[40px] font-[700] text-[#EDF1F3] 2xl:text-left">
+            What we're <br />
+            creating
+          </h1>
+          <p className="text-center text-[20px] font-[400] text-[#EDF1F3] 2xl:text-left">
+            Kneeshaw Game Development is a leading game development company that
+            specializes in creating immersive and engaging gaming experiences
+            for players around the world. Based in the United Kingdom, Kneeshaw
+            Game Development is known for its passion for gaming and its
+            commitment to innovation, which has led to the creation of some of
+            the most popular and successful games on the market.
+          </p>
+        </div>
+        <div className="flex-center flex w-1/2">
+          <img
+            src="/images/aboutus_image_02.svg"
+            className="ml-auto hidden 2xl:block"
+          />
+          <img
+            src="/images/aboutus_image_04.svg"
+            className="mr-[-800px] mt-[-1000px] hidden 2xl:block"
+          />
+          <img
+            src="/images/aboutus_image_05.svg"
+            className="ml-auto mt-[-400px] hidden 2xl:block"
+          />
+        </div>
+      </section>
+
+      <section className="mt-[150px] flex flex-col items-center">
+        <h1 className="text-center text-[40px] font-[700] text-[#EDF1F3]">
+          Why choose us in
+          <br />
+          your game development?
+        </h1>
+        <img
+          src="/images/aboutus_lineimage_01.svg"
+          className="mt-[21px] w-[100px]"
+        />
+      </section>
+
+      <section className="mt-[120px] flex h-[278px] justify-center gap-[50px] font-[400] text-[#EDF1F3]">
+        <div className="flex w-[400px] flex-col gap-[30px]">
+          <img
+            src="/images/aboutus_icon_1.PNG"
+            className="h-[68px] object-scale-down"
+          />
+          <h1 className="text-center text-[30px]">Innovation</h1>
+          <p className="text-center text-[20px]">
+            Our company uses innovative game development techniques and
+            technologies.
+          </p>
+        </div>
+        <div className="flex w-[400px] flex-col gap-[30px]">
+          <img
+            src="/images/aboutus_icon_2.PNG"
+            className="h-[68px] object-scale-down"
+          />
+          <h1 className="text-center text-[30px]">Quality</h1>
+          <p className="text-center text-[20px]">
+            We prioritize quality in every aspect of game development, from art
+            to programming to testing
+          </p>
+        </div>
+        <div className="flex w-[400px] flex-col gap-[30px]">
+          <img
+            src="/images/aboutus_icon_3.PNG"
+            className="h-[68px] object-scale-down"
+          />
+          <h1 className="text-center text-[30px]">Expertise</h1>
+          <p className="text-center text-[20px]">
+            5+ years experience in game development industry.
+          </p>
+        </div>
+      </section>
+
+      <section className="mt-[160px] flex h-[278px] justify-center gap-[50px] font-[400] text-[#EDF1F3] lg:mt-[84px]">
+        <div className="flex w-[400px] flex-col gap-[30px]">
+          <img
+            src="/images/aboutus_icon_4.PNG"
+            className="h-[68px] object-scale-down"
+          />
+          <h1 className="text-center text-[30px]">Support</h1>
+          <p className="text-center text-[20px]">
+            After the game is launched any updates, bugs, or marketing support,
+            you can rely on our team.
+          </p>
+        </div>
+        <div className="flex w-[400px] flex-col gap-[30px]">
+          <img
+            src="/images/aboutus_icon_5.PNG"
+            className="h-[68px] object-scale-down"
+          />
+          <h1 className="text-center text-[30px]">Collaboration</h1>
+          <p className="text-center text-[20px]">
+            We’re committed to collaborating with our clients to ensure they get
+            the game they want.
+          </p>
+        </div>
+      </section>
+
+      <section className="mt-[126px] flex flex-col lg:flex-row">
+        <div className="w-1/2">
+          <img
+            src="/images/aboutus_image_03.svg"
+            className="ml-[-120px] hidden w-[339px] lg:block"
+          />
+        </div>
+        <div className="w-full text-[#EDF1F3] lg:w-1/2">
+          <img
+            src="/images/aboutus_lineimage_03.svg"
+            className="ml-auto object-right"
+          />
+          <h1 className="mt-[21px] text-center text-[40px] font-[700] lg:text-right">
+            Our mission
+          </h1>
+          <p className="mt-[50px] text-center text-[20px] font-[400] lg:text-right">
+            One of our main focuses is on creating games that are accessible to
+            players of all skill levels. We believe that games should be fun and
+            engaging for everyone, regardless of their experience with gaming,
+            and we work hard to ensure that our games are easy to pick up and
+            play while still providing a challenging and rewarding experience.
+            <br />
+            <br />
+            In addition to creating our own original games, we also work with
+            publishers and other developers to help bring their games to market.
+            Our team has extensive experience in all aspects of game
+            development, from concept and design to programming and marketing,
+            and we are committed to helping our clients achieve success in the
+            highly competitive gaming industry.
+          </p>
+        </div>
+      </section>
+
+      <section className="mt-[150px] flex flex-col items-center text-[40px] font-[700] text-[#EDF1F3]">
+        Our Team
+        <img
+          src="/images/aboutus_lineimage_01.svg"
+          className="mt-[21px] w-[100px]"
+        />
+      </section>
+
+      <section className="mt-[108px] w-full text-center text-[40px] font-[400] text-[#EDF1F3] xl:w-1/2">
+        Kneeshaw Developments Team
+      </section>
+
+      <section className="mt-[40px] flex flex-col text-center font-[400] text-[#EDF1F3] xl:flex-row">
+        <div className="flex w-full flex-col xl:w-1/2">
+          <p className="text-[20px]">
+            Kneeshaw Game Development is home to a team of experienced and
+            dedicated professionals who are passionate about creating innovative
+            and engaging games for players around the world. Our development
+            team is made up of skilled programmers, designers, artists, and
+            writers who work together to bring our games to life. With their
+            expertise in various areas of game development, our team is able to
+            tackle complex challenges and create games that are both fun to play
+            and visually stunning.
+          </p>
+        </div>
+        <div className="mt-[20px] flex w-full flex-row justify-center gap-[100px] text-[#EDF1F3] xl:mt-[0] xl:w-1/2">
+          <div className="flex h-[208px] flex-col gap-[10px]">
+            <img
+              src="/images/avatar_01.png"
+              className="h-[120px] object-scale-down"
+            />
+            <p className="text-[20px] font-[700]">Michael Kneeshaw</p>
+            <p className="text-[20px] font-[400]">
+              <i className="fa-solid fa-l fa-user-tie mr-[10px]" />
+              Lead
+            </p>
+          </div>
+          <div className="flex h-[208px] flex-col gap-[10px]">
+            <img
+              src="/images/avatar_02.png"
+              className="h-[120px] object-scale-down"
+            />
+            <p className="text-[20px] font-[700]">Aidan Abat</p>
+            <p className="text-[20px] font-[400]">
+              <i className="fa-solid fa-l fa-user-gear mr-[10px]" />
+              Developer
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-[180px] flex flex-col justify-center gap-[20px] px-[30px] xl:flex-row">
+        <div className="w-full xl:w-1/2">
+          <img
+            src="/images/aboutus_lineimage_02.svg"
+            className="mt-[-30px] w-[100px]"
+          />
+          <h1 className="mt-[21px] text-[40px] font-[700] text-[#EDF1F3]">
+            Contact Us
+          </h1>
+          <p className="mt-[80px] text-left text-[30px] font-[400] text-[#EDF1F3]">
+            Turning Your Ideas and
+            <br /> Fantasies Into Playable Games
+          </p>
+        </div>
+        <div className=" flex w-full flex-col gap-[20px] bg-[#292929] p-[35px] xl:w-1/2">
+          <p className="text-[20px] font-[400] text-[#EDF1F3]">
+            Let us know about your project
+          </p>
+          <input
+            className="h-[52px] w-full rounded-sm border-[1px] bg-[#292929] p-[5px] font-[300] text-[#C2B9B0]"
+            placeholder="Full name*"
+          />
+          <input
+            className="h-[52px] w-full rounded-sm border-[1px] bg-[#292929] p-[5px] font-[300] text-[#C2B9B0]"
+            placeholder="Email Address*"
+          />
+          <input
+            className="h-[165px] w-full rounded-sm border-[1px] bg-[#292929] p-[5px] font-[300] text-[#C2B9B0]"
+            placeholder="Write your message"
+          />
+          <button className="h-[52px] rounded-lg bg-[#AFD275] text-[18px] font-[600] text-[#EDF1F3]">
+            Send Message
+          </button>
+        </div>
+      </section>
+
+      <section className=" mt-[38px] text-[20px] font-[400]">Stalk us</section>
+
+      <section className="mb-[20px] mt-[32px] flex flex-row gap-[20px] text-[#EDF1F3]">
+        <i className="fa-brands fa-xl fa-youtube" />
+        <i className="fa-brands fa-xl fa-steam" />
+        <i className="fa-brands fa-xl fa-reddit" />
+        <i className="fa-solid fa-xl fa-bolt-lightning" />
+        <i className="fa-brands fa-xl fa-discord" />
+        <i className="fa-brands fa-xl fa-twitch" />
+        <i className="fa-brands fa-xl fa-twitter" />
+      </section>
+
+      <Footer />
+    </main>
+  );
+}
