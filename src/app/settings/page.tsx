7aad8d863127176a2d613aaa9d0b4e02dd1dfc49
truthfuldev <truthfuldev@gmail.com>
2023-11-16 10:33:21 +0100

    create profile pages

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -5,17 +5,17 @@ import Footer from "@/components/layout/Footer";
 import { SITE_TITLE } from "@/utils/constants";
 
 export const metadata: Metadata = {
-  title: `${SITE_TITLE} - Cookies`,
-  description: `${SITE_TITLE} - Cookies`,
+  title: `${SITE_TITLE} - Settings`,
+  description: `${SITE_TITLE} - Settings`,
 };
 
-export default function Cookies() {
+export default function Settings() {
   return (
     <main className="flex min-h-screen w-full flex-col bg-[#211D19] text-white">
       <Header />
 
       <section>
-        <h1>Cookies</h1>
+        <h1>Settings</h1>
       </section>
 
       <Footer />
