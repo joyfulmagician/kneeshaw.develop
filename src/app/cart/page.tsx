deab4ee9a496fc6d006920a3aa1bf206ddb482cf
truthfuldev <truthfuldev@gmail.com>
2023-11-16 23:34:24 +0100

    create cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import { SITE_TITLE } from "@/utils/constants";
+import CartWizard from "@/components/cart";
 
 export const metadata: Metadata = {
   title: `${SITE_TITLE} - Cart`,
@@ -14,9 +15,7 @@ export default function Cart() {
     <main className="flex min-h-screen w-full flex-col bg-[#211D19] text-white">
       <Header />
 
-      <section>
-        <h1>Cart</h1>
-      </section>
+      <CartWizard />
 
       <Footer />
     </main>
