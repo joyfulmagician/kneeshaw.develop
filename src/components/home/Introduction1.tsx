a501e1373b71af4ded04fe524e65fa90fd06c763
truthfuldev <truthfuldev@gmail.com>
2023-11-16 09:51:22 +0100

    recode to components on home

---
+++ src/components/home/Introduction1.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function Introduction1() {
+  return (
+    <div className="grid grid-cols-12">
+      <div className="col-span-9 border-[1px] border-[#E7717D]" />
+      <div className="col-span-9 my-[30px] ml-[40px]">
+        Welcome to Kneeshaw Developments, the premier gaming development website
+        where you'll find the best games for gamers of all ages. Our passion for
+        gaming is evident in every game we create, and we are dedicated to
+        providing our players with immersive and engaging experiences that will
+        keep them coming back for more.
+      </div>
+      <div className="col-start-1 col-end-5 border-[1px] border-[#E7717D]" />
+    </div>
+  );
+}
