994e6fd8fd974691234ad47f8a2644868201d181
truthfuldev <truthfuldev@gmail.com>
2023-11-16 10:09:43 +0100

    refactor code

--- src/components/games/OldSalt.tsx
+++ src/components/games/OldSalt.tsx
@@ -4,17 +4,19 @@ import Link from "next/link";
 
 export default function OldSalt() {
   return (
-    <section className="flex flex-col gap-[40px] rounded-xl bg-[#2E2322] p-[50px] text-[#EDF1F3]">
+    <div className="flex flex-col gap-[40px] rounded-xl bg-[#2E2322] p-[50px] text-[#EDF1F3]">
       <h5 className="flex justify-end text-[20px] font-[600]">
         7k Players
         <span className="inline-flex h-3 w-3 rounded-full bg-[#9F0808]"></span>
       </h5>
+
       <div className="mb-[-100px] ml-[-180px] mt-[-100px] hidden lg:block">
         <img
           className="lg:h-[433px] lg:w-[1450px]"
           src="/images/games/image03.png"
         />
       </div>
+
       <div className="flex justify-end text-[20px]">
         <p className="w-[700px] text-right text-[20px] font-[400]">
           The game is a survival adventure that requires the player to manage
@@ -22,6 +24,7 @@ export default function OldSalt() {
           island for resources and materials to help them survive.
         </p>
       </div>
+
       <div className="flex flex-col justify-center gap-[20px] font-[600] sm:flex-row sm:justify-end">
         <Link href="/games/apply/id=2_get">
           <button className="flex h-[44px] w-[180px] items-center justify-center rounded-lg bg-[#9F0808] px-[44px] py-[12px] text-[16px] sm:w-[210px]">
@@ -29,11 +32,11 @@ export default function OldSalt() {
           </button>
         </Link>
         <Link href="/games/apply/id=2_more">
-          <button className="flex h-[44px] w-[180px] items-center justify-center rounded-lg border border-white bg-transparent px-[24px] py-[12px] text-[16px] sm:w-[210px] sm:w-[83px]">
+          <button className="flex h-[44px] w-[180px] items-center justify-center rounded-lg border border-white bg-transparent px-[24px] py-[12px] text-[16px] sm:w-[83px]">
             More
           </button>
         </Link>
       </div>
-    </section>
+    </div>
   );
 }
--- src/components/games/TitanSaga.tsx
+++ src/components/games/TitanSaga.tsx
@@ -4,22 +4,25 @@ import Link from "next/link";
 
 export default function TitanSaga() {
   return (
-    <section className="flex flex-col rounded-xl bg-[#3F3046] p-8 text-[#EDF1F3] xl:flex-row">
+    <div className="flex flex-col rounded-xl bg-[#3F3046] p-8 text-[#EDF1F3] xl:flex-row">
       <div className="flex w-full flex-col gap-8 xl:w-1/2">
         <h5 className="flex justify-start text-[20px] font-[600]">
           7k Players
           <span className="inline-flex h-3 w-3 rounded-full bg-[#B74AFF]"></span>
         </h5>
+
         <img
           className="mb-[-100px] mt-[-100px] hidden h-[369.8px] w-[562px] object-scale-down lg:block"
           src="/images/games/image01.png"
         />
+
         <p className="text-[20px] font-[400]">
           It is a turn-based RPG (role-playing game) that takes place in a
           fantasy world filled with magic and mythical creatures. The game
           features a deep and immersive story, with players taking on the role
           of a hero tasked with saving the world from destruction.
         </p>
+
         <div className="flex flex-col justify-center gap-[20px] sm:flex-row sm:justify-start">
           <Link href="/games/apply/id=1_get">
             <button className="flex h-[44px] w-[180px] items-center justify-center rounded-lg bg-[#B74AFF] px-[44px] py-[12px] text-[16px] text-base font-[600] sm:w-[210px]">
@@ -27,16 +30,17 @@ export default function TitanSaga() {
             </button>
           </Link>
           <Link href="/games/apply/id=1_more">
-            <button className="flex h-[44px] w-[180px] items-center justify-center rounded-lg border border-white bg-transparent px-[24px] py-[12px] text-[16px] font-[600] sm:w-[210px] sm:w-[83px]">
+            <button className="flex h-[44px] w-[180px] items-center justify-center rounded-lg border border-white bg-transparent px-[24px] py-[12px] text-[16px] font-[600] sm:w-[83px]">
               More
             </button>
           </Link>
         </div>
       </div>
+
       <img
         src="/images/games/image02.png"
         className="mt-[-150px] hidden h-[623px] w-[600px] object-scale-down xl:block"
       />
-    </section>
+    </div>
   );
 }
