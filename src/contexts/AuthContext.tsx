92af7cc5e1b84a6d4605e9d8a46fcc56b696c337
truthfuldev <truthfuldev@gmail.com>
2023-11-16 18:54:18 +0100

    implement auth context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-import { AUTH_STATUS } from "@/utils/constants";
 import React, { createContext, useContext, useState } from "react";
+
+import { AUTH_STATUS } from "@/utils/constants";
+import { IUser } from "@/utils/types";
 
 interface ContextProps {
   status: string;
   setStatus: (newStatus: string) => void;
-  user: object;
-  setUser: (newUser: object) => void;
+  user: IUser | null;
+  setUser: (newUser: IUser) => void;
 }
 
 const defaultContextValue: ContextProps = {
-  status: AUTH_STATUS.CHECKING,
+  status: AUTH_STATUS.NO_AUTHENTICATED,
   setStatus: (newStatus: string) => {},
-  user: {},
-  setUser: (newUser: object) => {},
+  user: null,
+  setUser: (newUser: IUser) => {},
 };
 
 export const AuthContext = createContext<ContextProps>(defaultContextValue);
@@ -28,8 +30,17 @@ interface ProviderProps {
 }
 
 export default function AuthProvider({ children }: ProviderProps) {
-  const [status, setStatus] = useState(AUTH_STATUS.CHECKING);
-  const [user, setUser] = useState({});
+  const [status, setStatus] = useState(AUTH_STATUS.NO_AUTHENTICATED);
+  const [user, setUser] = useState<IUser | null>(null);
+
+  // TODO: Test
+  // const [status, setStatus] = useState(AUTH_STATUS.AUTHENTICATED);
+  // const [user, setUser] = useState<IUser>({
+  //   uid: "4ac4-abcd-123f-123a",
+  //   firstName: "Emery",
+  //   lastName: "Stanton",
+  //   email: "emerystanton@gmail.com",
+  // });
 
   return (
     <AuthContext.Provider value={{ status, setStatus, user, setUser }}>
---
+++ src/utils/types.ts
@@ -0,0 +1,6 @@
+export interface IUser {
+  uid: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
